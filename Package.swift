d3cceccb7297b1da40a855fe1f50e1a2cf5e3347
Aref <arefshal@yahoo.com>
2024-12-25 04:05:03 +0330

    feat: Add Lottie dependency for enhanced animations support

--- Package.swift
+++ Package.swift
@@ -5,10 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "Crypto",
+    platforms: [
+        .iOS(.v14),      
+        .macOS(.v10_15)  
+    ],
+    dependencies: [
+        .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.5.0") // اضافه کردن Lottie
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "Crypto"),
+            name: "Crypto",
+            dependencies: [
+                .product(name: "Lottie", package: "lottie-ios") // وابستگی به Lottie
+            ]
+        )
     ]
 )
